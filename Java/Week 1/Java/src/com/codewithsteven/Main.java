29b05e3e7555b6b4fc2ab6910fc832890994c40b
steven.kyaw <steven.kyaw@gmail.com>
2024-04-03 14:55:21 +0800

    Initial Java Commit

---
+++ Java/Week 1/Java/src/com/codewithsteven/Main.java
@@ -0,0 +1,11 @@
+package com.codewithsteven;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // ctrl + space bar to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello World");
+    }
+}
